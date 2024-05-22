14c48b815c148bea77526333e08e5d4481c922a8
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2024-05-22 10:09:35 -0300

    añadiendo watch con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,16 @@
-export function
+import gulpSass from 'gulp-sass'
+import * as dartSass from 'sass'
+import {src, dest, watch} from 'gulp'
+const sass = gulpSass(dartSass)
+
+export function css( done ) {
+    src('src/scss/app.scss')
+        .pipe( sass() )
+        .pipe( dest('build/css'))
+
+    done()
+}
+
+export function dev() {
+    watch('src/scss/app.scss', css)
+}
