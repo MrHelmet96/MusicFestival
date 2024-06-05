c99560d1da1517369f478b77541fcc6148d93584
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2024-06-05 09:20:27 -0300

    modal y setTimeOut en galeria

--- src/js/app.js
+++ src/js/app.js
@@ -11,6 +11,40 @@ function crearGaleria() {
         imagen.src = `src/img/gallery/full/${i}.jpg`
         imagen.alt = 'Imagen Galeria'
 
+        //Event Handler
+        imagen.onclick = function() {
+            mostrarImagen(i)
+        }
+
         galeria.appendChild(imagen)
     }
+}
+
+function mostrarImagen(i) {
+    const imagen = document.createElement('IMG')
+    imagen.src = `src/img/gallery/full/${i}.jpg`
+    imagen.alt = 'Imagen Galeria'
+
+    //generar Modal
+    const modal = document.createElement('DIV')
+    modal.classList.add('modal')
+    modal.onclick = cerrarModal
+    modal.appendChild(imagen)
+
+    //agregar al HTML
+    const body = document.querySelector('body')
+    body.classList.add('overflow-hidden')
+    body.appendChild(modal);
+}
+
+function cerrarModal() {
+    const modal = document.querySelector('.modal')
+    modal.classList.add('fade-out')
+
+    setTimeout(()=>{
+        modal?.remove()
+
+        const body = document.querySelector('body')
+        body.classList.remove('overflow-hidden')
+    }, 450)
 }
