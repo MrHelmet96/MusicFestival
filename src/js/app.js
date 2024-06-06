29772bd19a11524b79e37b2b7288603b1ee440b9
Alejandro Matías Casco <alejandromatiascasco@gmail.com>
2024-06-06 16:35:43 -0300

    Funcionalidad de navegación

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', function(){
+    navFija();
     crearGaleria();
+    highlightNavLink();
 })
+
+function navFija(){
+    const header = document.querySelector('.header')
+    const sobreFestival = document.querySelector('.sobre-festival')
+
+    window.addEventListener('scroll', function(){
+        if(sobreFestival.getBoundingClientRect().bottom < 1) {
+            header.classList.add('fixed')
+        } else {
+            header.classList.remove('fixed')
+        }
+    })
+}
 
 function crearGaleria() {
     const galeria = document.querySelector('.galeria-imagenes');
@@ -31,6 +46,13 @@ function mostrarImagen(i) {
     modal.onclick = cerrarModal
     modal.appendChild(imagen)
 
+    //Botón cerrar modal
+    const cerrarModalBtn = document.createElement('BUTTON')
+    cerrarModalBtn.textContent = 'X'
+    cerrarModalBtn.classList.add('btn-cerrar')
+    cerrarModalBtn.onclick = cerrarModal
+    modal.appendChild(cerrarModalBtn)
+
     //agregar al HTML
     const body = document.querySelector('body')
     body.classList.add('overflow-hidden')
@@ -47,4 +69,29 @@ function cerrarModal() {
         const body = document.querySelector('body')
         body.classList.remove('overflow-hidden')
     }, 450)
+}
+
+function highlightNavLink() {
+    document.addEventListener('scroll', function(){
+        const sections = document.querySelector('section')
+        const navLinks = document.querySelector('.navegacion-principal a')
+
+        let actual = ''
+
+        sections.forEach(section => {
+            const sectionTop = section.offserTop
+            const sectionHeight = section.clientHeight
+
+            if(window.scrollY >= (sectionTop - sectionHeight / 3)){
+                actual = section.id;
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove('active')
+            if(link.getAttribute('href') === '#' + actual){
+                link.classList.add('active')
+            }
+        })
+    })
 }
